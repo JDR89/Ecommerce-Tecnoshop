cc48c15ded5eb73a4142e7db7695b30d0f8f9990
JDR89 <joaquin.romero89@gmail.com>
2024-04-24 16:43:10 -0300

    try y catch a categoria productos

--- src/app/(shop)/productos/[categoria]/page.js
+++ src/app/(shop)/productos/[categoria]/page.js
@@ -32,40 +32,53 @@ const getProducts = async(category) => {
 
 export default async function ProductosPage({params}) {
   const {categoria}=params
-  const products = await getProducts(categoria)
+
+  try{
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/productos/${categoria}`,{
+      cache: "no-store"
+    });
+    const products = await resp.json();
+
+    return (
+      <div>
+        <div>
+          <div
+            className="hero h-[10rem] lg:h-[18rem] mb-6 "
+            style={{
+              backgroundImage:
+                "url(https://resource.logitech.com/w_1800,h_1800,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/homepage/static-hpb/casa-hpb-desktop.jpg?v=1)",
+            }}
+          >
+            <div className="">
+              <h1 className="text-3xl lg:text-6xl font-bold text-warning-content capitalize">
+                {
+                  (categoria === "todo") ? "Productos" : categoria
+                }
+              </h1>
+            </div>
+          </div>
+        </div>
+  
+            
+        <MenuCategorias />
+  
+                <Suspense fallback={<div>Loading...</div>}>
+                
+                    <ProductList products={products} />  
+                  
+                   
+                </Suspense>
+        
+      </div>
+    );
+  } catch (error) { 
+    console.log(error,"error al cargar los datos")
+    return <div>Error al cargar los datos</div>;
+  }
+  }
+  
 
   
 
-  return (
-    <div>
-      <div>
-        <div
-          className="hero h-[10rem] lg:h-[18rem] mb-6 "
-          style={{
-            backgroundImage:
-              "url(https://resource.logitech.com/w_1800,h_1800,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/homepage/static-hpb/casa-hpb-desktop.jpg?v=1)",
-          }}
-        >
-          <div className="">
-            <h1 className="text-3xl lg:text-6xl font-bold text-warning-content capitalize">
-              {
-                (categoria === "todo") ? "Productos" : categoria
-              }
-            </h1>
-          </div>
-        </div>
-      </div>
+  
 
-          
-      <MenuCategorias />
-
-              <Suspense fallback={<div>Loading...</div>}>
-              
-                  <ProductList products={products} />  
-                
-                 
-              </Suspense>
-      
-    </div>
-  );
-}
